ab6d5bf558a821d3be278dd2af683c11cf8340fb
Josua (Josh) Tjiauw <tjiauwj675@gmail.com>
2024-07-09 11:10:48 -0700

    lab exercise 1

--- SafeWalkVolunteer/Volunteer.swift
+++ SafeWalkVolunteer/Volunteer.swift
@@ -12,6 +12,24 @@ protocol Volunteer: ObservableObject {
     var maxHours: Int { get }
 }
 
+class SafeWalkVolunteer : Volunteer, ObservableObject{
+    var name: String
+    var age: Int
+    var maxHours: Int{
+        if age < 18{
+            return 1
+        }
+        return 3
+    }
+    init() {
+        self.name=""
+        self.age=0
+    }
+    init(name : String, age : Int){
+        self.name=name
+        self.age=age
+    }
+}
 // TODO: Create a class called SafeWalkVolunteer that implementes the Volunteer protocol.
 
 // TODO: Create an Int maxHours computed property. It is a get-only property that returns 1 if the volunteer's age is less than 18 and returns 3 if the volunteer's age is 18 and over.
